c00d10384ec6c69b73a0540eeab185046b0f0c38
Gilson Ferreira <gilsonfsdev@gmail.com>
2023-09-20 17:34:35 -0300

    feat prisma gyms and users repositories

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,10 +3,10 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById(id: string) {
+  async findById(email: string) {
     const user = await prisma.user.findUnique({
       where: {
-        id,
+        email,
       },
     })
 
