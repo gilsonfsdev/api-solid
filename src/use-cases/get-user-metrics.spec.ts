212cb0f119aae3843c136601deebf094602f20fb
Gilson Ferreira <gilsonfsdev@gmail.com>
2023-09-27 09:05:21 -0300

    Update get-user-metrics.spec.ts

--- src/use-cases/get-user-metrics.spec.ts
+++ src/use-cases/get-user-metrics.spec.ts
@@ -22,10 +22,10 @@ describe('Get User Metrics Use Case', () => {
       user_id: 'user-01',
     })
 
-    const { checkInCount } = await sut.execute({
+    const { checkInsCount } = await sut.execute({
       userId: 'user-01',
     })
 
-    expect(checkInCount).toEqual(2)
+    expect(checkInsCount).toEqual(2)
   })
 })
